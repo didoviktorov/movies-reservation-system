944fa10bc7510cb40320294ce364f491f7934af5
Dido Viktorov <d.viktorovchak@gmail.com>
2018-04-04 19:58:07 +0300

    Just create applicaton

---
+++ src/main/java/moviessystem/moviesreservation/ServletInitializer.java
@@ -0,0 +1,13 @@
+package moviessystem.moviesreservation;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MoviesreservationApplication.class);
+	}
+
+}
